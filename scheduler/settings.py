7f1b557c6b97ab6f996a38cffc260d14d55dd9b2
kmhoran <kevin.michael.horan@gmail.com>
2018-04-22 11:57:59 -0700

    change up settings

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -134,7 +134,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
  
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'site/assets')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
  
 # Extra places for collectstatic to find static files.
