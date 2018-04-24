67c4ca34c420cd5f545b0d7001c949a6391cac8c
kmhoran <kevin.michael.horan@gmail.com>
2018-04-24 12:32:27 -0700

    change up settings again

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
  
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
