3084c04252fe22ad96e3326ef43c81580c38ada2
kmhoran <kevin.michael.horan@gmail.com>
2018-04-26 06:44:43 -0700

    switch to debug

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['la-weather-app.herokuapp.com']
 
