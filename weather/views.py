01990683086174f26b1c8c9a904e88dbfe8abc7a
kmhoran <kevin.michael.horan@gmail.com>
2018-04-25 20:47:35 -0700

    realize this is python

--- weather/views.py
+++ weather/views.py
@@ -13,9 +13,9 @@ class MainPage(TemplateView):
         latest_forecast = Forecast.objects.latest('timestamp')
         
         # Because Heroku Hobby Tier goes to sleep after inactivity
-        # if(not latest_forecast or latest_forecast.timestamp < (datetime.now() - timedelta(hours=1))):
-        #     forecast_api.update_forecast()
-        #     latest_forecast = Forecast.objects.latest('timestamp')
+        if not latest_forecast or latest_forecast.timestamp < (datetime.now() - timedelta(hours=1)):
+            forecast_api.update_forecast()
+            latest_forecast = Forecast.objects.latest('timestamp')
 
         city = latest_forecast.city
         temperature_in_c = latest_forecast.temperatue
