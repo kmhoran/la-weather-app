04ed177a744b791a663e1801a483f525dba44193
Kevin Horan <kevin.michael.horan@gmail.com>
2018-04-27 20:01:06 -0700

    Update updater.py

--- forecastUpdater/updater.py
+++ forecastUpdater/updater.py
@@ -4,16 +4,9 @@ import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from forecastUpdater import forecast_api
 
-
-def test():
-    now = datetime.now()
-    print("\nTEST AT {t.hour}:{t.minute}:{t.second}\n".format(
-        t=now
-    ))
         
 def start():
         scheduler = BackgroundScheduler()
-        # scheduler.configure(jobstores=jobstores)
         scheduler.add_job(forecast_api.update_forecast, 'interval', minutes=45)
         scheduler.start()
     
